475cec3c35fdbb30dc2670d1172bb34d83628224
stedelia <144045955+stedelia@users.noreply.github.com>
2025-03-20 12:28:38 +0100

    fix: promote to UAT (#28)
    Co-authored-by: Vitolo-Andrea <andrea.vitolo@emeal.nttdata.com>
    Co-authored-by: andreaV95 <157486351+Vitolo-Andrea@users.noreply.github.com>
    Co-authored-by: BaldiVi <vittorio.baldi@emea.nttdata.com>
    Co-authored-by: BaldiVi-1 <vittorio.baldi@emeal.nttdata.com>

--- src/test/java/it/gov/pagopa/onboarding/citizen/service/BloomFilterInitializerTest.java
+++ src/test/java/it/gov/pagopa/onboarding/citizen/service/BloomFilterInitializerTest.java
@@ -1,6 +1,9 @@
 package it.gov.pagopa.onboarding.citizen.service;
 
+import it.gov.pagopa.onboarding.citizen.model.CitizenConsent;
+import it.gov.pagopa.onboarding.citizen.model.ConsentDetails;
 import it.gov.pagopa.onboarding.citizen.repository.CitizenRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
@@ -9,87 +12,94 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.redisson.api.RBloomFilterReactive;
 import org.redisson.api.RLockReactive;
 import org.redisson.api.RedissonReactiveClient;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
+
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyDouble;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.*;
 
-
 @ExtendWith({SpringExtension.class, MockitoExtension.class})
+@SuppressWarnings({"unchecked", "rawtypes"})
 class BloomFilterInitializerTest {
 
+    private static final String REDDIS_BF_NAME = "emd-bloom-filter";
     private static final String REDIS_LOCK_NAME = "startup-task-lock";
-    private static final String REDDIS_BF_NAME = "emd-bloom-fiter";
 
-    @Mock
-    private RedissonReactiveClient redissonClient;
+
+    private RBloomFilterReactive bloomFilter;
 
     @Mock
     private RLockReactive lock;
 
-    @Mock
+    @MockBean
+    private RedissonReactiveClient redissonClient;
+
+    @MockBean
     private CitizenRepository citizenRepository;
 
     private BloomFilterInitializer bloomFilterInitializer;
 
-    @Test
-    void test(){
+    @BeforeEach
+    void setUp() {
+        bloomFilter = mock(RBloomFilterReactive.class);
         MockitoAnnotations.openMocks(this);
+
+        when(redissonClient.getBloomFilter(REDDIS_BF_NAME)).thenReturn(bloomFilter);
         when(redissonClient.getLock(REDIS_LOCK_NAME)).thenReturn(lock);
+        when(lock.tryLock(0, 60, TimeUnit.SECONDS)).thenReturn(Mono.just(true));
+        bloomFilterInitializer = new BloomFilterInitializer(redissonClient, citizenRepository);
+    }
 
-        RBloomFilterReactive<?> bloomFilter = mock(RBloomFilterReactive.class);
+    private void mockCommonDependencies() {
+        when(bloomFilter.contains("fiscalCode")).thenReturn(Mono.just(true));
+        when(lock.tryLock(0, 60, TimeUnit.SECONDS)).thenReturn(Mono.just(true));
 
-        when(redissonClient.getBloomFilter(REDDIS_BF_NAME)).thenReturn((RBloomFilterReactive<Object>) bloomFilter);
+        Map<String, ConsentDetails> consents = new HashMap<>();
+        consents.put("test", ConsentDetails.builder().tppState(true).build());
 
-        when(bloomFilter.tryInit(anyLong(), anyDouble())).thenReturn(Mono.empty());
-        bloomFilterInitializer = new BloomFilterInitializer(redissonClient,citizenRepository);
-        StepVerifier.create(bloomFilter.tryInit(1000L, 0.01))
-                .verifyComplete();
-
-        when(citizenRepository.findAll()).thenReturn(Flux.empty());
-        StepVerifier.create(citizenRepository.findAll())
-                .verifyComplete();
-
-        when(lock.tryLock(anyLong(), anyLong(), any(TimeUnit.class)))
-                .thenReturn(Mono.just(true));
-
-        bloomFilterInitializer.initializer();
-
-        verify(lock).tryLock(0, 60, TimeUnit.SECONDS);
-
+        when(bloomFilter.isExists()).thenReturn(Mono.just(true));
+        when(citizenRepository.findAll()).thenReturn(Flux.just(
+                CitizenConsent.builder()
+                        .id("id")
+                        .fiscalCode("fiscalCode")
+                        .consents(consents)
+                        .build()
+        ));
+        when(bloomFilter.add(anyString())).thenReturn(Mono.just(true));
     }
 
     @Test
-    void test2(){
-        MockitoAnnotations.openMocks(this);
-        when(redissonClient.getLock(REDIS_LOCK_NAME)).thenReturn(lock);
+    void testInitialize() {
+        mockCommonDependencies();
 
-        RBloomFilterReactive<?> bloomFilter = mock(RBloomFilterReactive.class);
+        bloomFilterInitializer.initialize();
 
-        when(redissonClient.getBloomFilter(REDDIS_BF_NAME)).thenReturn((RBloomFilterReactive<Object>) bloomFilter);
-
-        when(bloomFilter.tryInit(anyLong(), anyDouble())).thenReturn(Mono.empty());
-        bloomFilterInitializer = new BloomFilterInitializer(redissonClient,citizenRepository);
-        StepVerifier.create(bloomFilter.tryInit(1000L, 0.01))
+        StepVerifier.create(bloomFilter.contains("fiscalCode"))
+                .expectNext(true)
                 .verifyComplete();
+    }
 
-        when(citizenRepository.findAll()).thenReturn(Flux.empty());
-        StepVerifier.create(citizenRepository.findAll())
+    @Test
+    void testResetBloomFilter() {
+        mockCommonDependencies();
+        when(bloomFilter.delete()).thenReturn(Mono.empty());
+        when(bloomFilter.tryInit(1000L, 0.01)).thenReturn(Mono.just(true));
+
+        bloomFilterInitializer.resetBloomFilter();
+
+        StepVerifier.create(bloomFilter.contains("fiscalCode"))
+                .expectNext(true)
                 .verifyComplete();
-
-        when(lock.tryLock(anyLong(), anyLong(), any(TimeUnit.class)))
-                .thenReturn(Mono.just(false));
-
-        bloomFilterInitializer.initializer();
-
-        verify(lock).tryLock(0, 60, TimeUnit.SECONDS);
-
     }
 }
+
+
 
