391da3d54ebb0bafa3b0eae53d1dfd68701268ea
stefanodel <stefano11.delia@nttdata.com>
2025-03-24 16:54:03 +0100

    chore: depcheck upgrade

--- src/main/java/it/gov/pagopa/onboarding/citizen/controller/CitizenController.java
+++ src/main/java/it/gov/pagopa/onboarding/citizen/controller/CitizenController.java
@@ -61,5 +61,4 @@ public interface CitizenController {
     @DeleteMapping("/test/delete/{fiscalCode}")
     Mono<ResponseEntity<CitizenConsentDTO>> deleteCitizenConsent(@PathVariable @Pattern(regexp = FISCAL_CODE_STRUCTURE_REGEX, message = "Invalid fiscal code format") String fiscalCode);
 
-
 }
