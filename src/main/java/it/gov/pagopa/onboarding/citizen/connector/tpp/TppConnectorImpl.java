430ab18a444eb9b16b2c1accdeefab4bdb7cdacc
Marco-Meloni-30 <marco.meloni@emeal.nttdata.com>
2026-03-04 14:56:05 +0100

    feat: MMC-357 update endpoint of tpp connector (#85)

--- src/main/java/it/gov/pagopa/onboarding/citizen/connector/tpp/TppConnectorImpl.java
+++ src/main/java/it/gov/pagopa/onboarding/citizen/connector/tpp/TppConnectorImpl.java
@@ -50,10 +50,10 @@ public class TppConnectorImpl implements TppConnector {
     /**
      * {@inheritDoc}
      *
-     * <p>Performs a reactive HTTP POST to {@code /emd/tpp/list} with the provided TPP IDs.</p>
+     * <p>Performs a reactive HTTP POST to {@code /emd/tpp/list} with the provided TPP IDs and recipientId.</p>
      */
     @Override
-    public Mono<List<TppDTO>> getTppsEnabled(TppIdList tppIdList) {
+    public Mono<List<TppDTO>> filterEnabledList(TppIdList tppIdList) {
         return webClient.post()
                 .uri("/emd/tpp/list")
                 .bodyValue(tppIdList)
